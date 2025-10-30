3ed5dcd29333cfbb3eeb0a3479eac7f69c7a0ed7
Ben Severns <severns3@gmail.com>
2025-10-29 21:52:01 -0500

    Build meta
    platform.ini lib includes

--- include/BuildInfo.h
+++ include/BuildInfo.h
@@ -1,11 +1,3 @@
 #pragma once
-
-// Build metadata stamped in at compile time.  Great for showing students how to
-// surface git SHA + build timestamps on a microcontroller without blowing flash
-// budgets.
-#ifndef SEEDBOX_GIT
-#define SEEDBOX_GIT "nogit"
-#endif
-#ifndef SEEDBOX_BUILT
-#define SEEDBOX_BUILT "unknown"
-#endif
+#define SEEDBOX_GIT "2fa3260"
+#define SEEDBOX_BUILT "2025-10-29 18:26:05"
