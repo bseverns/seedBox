11af76562978f75d4c561c70e4a9682d8a556384
Ben Severns <severns3@gmail.com>
2025-12-11 11:52:13 -0600

    Use choice parameters for combo controls

--- src/juce/SeedboxAudioProcessor.cpp
+++ src/juce/SeedboxAudioProcessor.cpp
@@ -244,13 +244,21 @@ juce::AudioProcessorValueTreeState::ParameterLayout SeedboxAudioProcessor::creat
   std::vector<std::unique_ptr<juce::RangedAudioParameter>> params;
   const Seed defaults{};
   params.push_back(std::make_unique<juce::AudioParameterInt>(kParamMasterSeed, "Master Seed", 0, 9999999, 1));
-  params.push_back(std::make_unique<juce::AudioParameterInt>(kParamFocusSeed, "Focus Seed", 0, 3, 0));
-  params.push_back(std::make_unique<juce::AudioParameterInt>(kParamSeedEngine, "Seed Engine", 0, 7, 0));
+  params.push_back(std::make_unique<juce::AudioParameterChoice>(
+      kParamFocusSeed, "Focus Seed",
+      juce::StringArray{"Seed 1", "Seed 2", "Seed 3", "Seed 4"}, 0));
+  params.push_back(std::make_unique<juce::AudioParameterChoice>(
+      kParamSeedEngine, "Seed Engine",
+      juce::StringArray{"Default", "Grain", "Chord", "Drum", "FM", "Additive", "Resonator", "Noise"}, 0));
   params.push_back(std::make_unique<juce::AudioParameterFloat>(
       kParamSwingPercent, "Swing Percent",
       juce::NormalisableRange<float>{0.0f, 0.99f, 0.01f, 1.0f, false}, 0.0f));
-  params.push_back(std::make_unique<juce::AudioParameterInt>(kParamQuantizeScale, "Quantize Scale", 0, 4, 0));
-  params.push_back(std::make_unique<juce::AudioParameterInt>(kParamQuantizeRoot, "Quantize Root", 0, 11, 0));
+  params.push_back(std::make_unique<juce::AudioParameterChoice>(
+      kParamQuantizeScale, "Quantize Scale",
+      juce::StringArray{"Chromatic", "Major", "Minor", "Dorian", "Lydian"}, 0));
+  params.push_back(std::make_unique<juce::AudioParameterChoice>(
+      kParamQuantizeRoot, "Quantize Root",
+      juce::StringArray{"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"}, 0));
   params.push_back(std::make_unique<juce::AudioParameterBool>(kParamTransportLatch, "Transport Latch", false));
   params.push_back(
       std::make_unique<juce::AudioParameterBool>(kParamClockSourceExternal, "Clock Source External", false));
