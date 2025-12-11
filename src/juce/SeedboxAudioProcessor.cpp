280298f507f2b2a102ad068e68449c5e0af1db5d
Ben Severns <severns3@gmail.com>
2025-12-11 11:53:19 -0600

    Use choice parameters for JUCE combo controls

--- src/juce/SeedboxAudioProcessor.cpp
+++ src/juce/SeedboxAudioProcessor.cpp
@@ -244,13 +244,24 @@ juce::AudioProcessorValueTreeState::ParameterLayout SeedboxAudioProcessor::creat
   std::vector<std::unique_ptr<juce::RangedAudioParameter>> params;
   const Seed defaults{};
   params.push_back(std::make_unique<juce::AudioParameterInt>(kParamMasterSeed, "Master Seed", 0, 9999999, 1));
-  params.push_back(std::make_unique<juce::AudioParameterInt>(kParamFocusSeed, "Focus Seed", 0, 3, 0));
-  params.push_back(std::make_unique<juce::AudioParameterInt>(kParamSeedEngine, "Seed Engine", 0, 7, 0));
+
+  juce::StringArray focusSeedChoices{"Seed 1", "Seed 2", "Seed 3", "Seed 4"};
+  params.push_back(std::make_unique<juce::AudioParameterChoice>(kParamFocusSeed, "Focus Seed", focusSeedChoices, 0));
+
+  juce::StringArray engineChoices{"Default", "Grain", "Chord", "Drum", "FM", "Additive", "Resonator", "Noise"};
+  params.push_back(std::make_unique<juce::AudioParameterChoice>(kParamSeedEngine, "Seed Engine", engineChoices, 0));
   params.push_back(std::make_unique<juce::AudioParameterFloat>(
       kParamSwingPercent, "Swing Percent",
       juce::NormalisableRange<float>{0.0f, 0.99f, 0.01f, 1.0f, false}, 0.0f));
-  params.push_back(std::make_unique<juce::AudioParameterInt>(kParamQuantizeScale, "Quantize Scale", 0, 4, 0));
-  params.push_back(std::make_unique<juce::AudioParameterInt>(kParamQuantizeRoot, "Quantize Root", 0, 11, 0));
+
+  juce::StringArray quantizeScaleChoices{"Chromatic", "Major", "Minor", "Dorian", "Lydian"};
+  params.push_back(
+      std::make_unique<juce::AudioParameterChoice>(kParamQuantizeScale, "Quantize Scale", quantizeScaleChoices, 0));
+
+  juce::StringArray quantizeRootChoices{"C",  "C#", "D",  "D#", "E",  "F",
+                                        "F#", "G",  "G#", "A",  "A#", "B"};
+  params.push_back(
+      std::make_unique<juce::AudioParameterChoice>(kParamQuantizeRoot, "Quantize Root", quantizeRootChoices, 0));
   params.push_back(std::make_unique<juce::AudioParameterBool>(kParamTransportLatch, "Transport Latch", false));
   params.push_back(
       std::make_unique<juce::AudioParameterBool>(kParamClockSourceExternal, "Clock Source External", false));
